50395e96842625da82e91a1cbc11b55c31d6d233
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-12-18 17:52:17 +0100

    Setup service for the projects page

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Personal Projects',
+    projectsText: 'Portofolio goes here'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Not found!'
